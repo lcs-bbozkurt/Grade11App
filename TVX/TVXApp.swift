a75bb4bd7b0ebc8c7a3af01ca7ab3f4964ea81e1
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-11-29 14:26:04 -0500

    Initial Commit

---
+++ TVX/TVXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVXApp.swift
+//  TVX
+//
+//  Created by Berk Bozkurt on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct TVXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
