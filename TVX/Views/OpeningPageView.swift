272e8108e636b92ed6a457bfb93cddfd6aa47c76
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-11-29 20:11:25 -0500

    The page is developed.
    Things to do first:
    	Navigation will be added.
    	Custom font will be added.

--- TVX/Views/OpeningPageView.swift
+++ TVX/Views/OpeningPageView.swift
@@ -10,25 +10,47 @@ import SwiftUI
 struct OpeningPageView: View {
     var body: some View {
         VStack(alignment: .leading){
+        
+            Spacer()
             Text("TVX")
-            Text("\"The new television provider\"")
+                .fontWeight(.black)
+                .foregroundColor(.black)
+                .font(.title3)
+                .italic()
             
+            Text("\"The new television provider\"")
+                .italic()
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
             Divider()
                 .padding(.vertical ,15)
-            Button(action: {print("button was pressed")}, label: {Text("Sign-up")})
+            HStack {
+            Spacer()
+                Button(action: {print("button was pressed")}, label: {Text("Sign-up")})
                 .foregroundColor(.primary)
-            Text ("or")
+                .buttonStyle(.bordered)
+            Text("or")
             Button(action: {print("button was pressed")}, label: {Text("Sign-in")})
                 .foregroundColor(.primary)
+                .buttonStyle(.bordered)
+                Spacer()
+            }
+            Divider()
+                .padding(.vertical ,30)
+            Text("TVX is the new era of satelitte providers. TVX helps you choose the best possible streaming subscribtions in a waaay reduced price.")
+                .fontWeight(.black)
+                .foregroundColor(.white)
+            Spacer()
+            
         }
-        .navigationTitle("TVX")
+    
+        .background(Image("background.orange").aspectRatio(contentMode: .fit).scaledToFit().blur(radius: 3))
     }
 }
 
 struct OpeningPageView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView() {
         OpeningPageView()
-        }
+        
     }
 }
